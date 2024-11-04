291ad92d050cbf3b3a5beed94be697c0000822d0
Mindaugas <mkarpinskas@exadel.com>
2024-11-05 00:12:00 +0200

    Task 4 branch 2

--- Task04/demo/src/main/java/lt/mxas/cources/task4/demo/DemoApplication.java
+++ Task04/demo/src/main/java/lt/mxas/cources/task4/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-
+        TestSpot.print();
         SpringApplication.run(DemoApplication.class, args);
 
     }
---
+++ Task04/demo/src/main/java/lt/mxas/cources/task4/demo/TestSpot.java
@@ -0,0 +1,11 @@
+package lt.mxas.cources.task4.demo;
+
+public class TestSpot {
+
+	private static Integer one;
+	public static void print(){
+		System.out.println("Hello World " + one++);
+
+	}
+
+}
