190860f76dd179fa8c813cf8859f6e36223f36c8
Mindaugas <mkarpinskas@exadel.com>
2024-11-04 22:52:18 +0200

    Task 4 checkstyle

--- Task04/demo/src/main/java/lt/mxas/cources/task4/demo/DemoApplication.java
+++ Task04/demo/src/main/java/lt/mxas/cources/task4/demo/DemoApplication.java
@@ -3,11 +3,14 @@ package lt.mxas.cources.task4.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Test comment.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
